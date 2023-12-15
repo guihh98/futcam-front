ed0e8942f74e4876db65aba8bf5ed5121092c8ef
henriqueambrosano <henriqueambrosano@hotmail.com>
2023-12-15 17:31:32 -0300

    criando componente para opções do menu settings

---
+++ src/components/menuOption/styles.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.div`
+    height: 30px;
+    width: 100%;
+    color: white;
+    margin: 20px 0px;
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+
+    &:hover {
+        cursor: pointer;
+        color: black;
+    }
+`

de399033c472103191da94a875227007de5936e8
henriqueambrosano <henriqueambrosano@hotmail.com>
2023-12-18 20:06:17 -0300

    adicionando botao voltar na settings

--- src/components/menuOption/index.tsx
+++ src/components/menuOption/index.tsx
@@ -5,13 +5,15 @@ import { IconDefinition } from "@fortawesome/free-solid-svg-icons"
 export interface MenuOptionProps {
     optionText: string,
     icon: IconDefinition,
+    color: string,
+    onClick?: () => void,
 }
 
-export const MenuOption = ({optionText, icon}: MenuOptionProps): JSX.Element => {
+export const MenuOption = ({optionText, icon, color, onClick}: MenuOptionProps): JSX.Element => {
     return (
-        <ButtonContainer>
-             <FontAwesomeIcon icon={icon} style={{color: "#ffffff"}} size={"2x"} />
-             <p className="mb-0 p-2 text-end" >{optionText}</p>
+        <ButtonContainer onClick={onClick}>
+             <FontAwesomeIcon icon={icon} style={{color}} size={"2x"} />
+             <p className="mb-0 p-2 text-end" style={{color}}>{optionText}</p>
         </ButtonContainer>
     )
 }

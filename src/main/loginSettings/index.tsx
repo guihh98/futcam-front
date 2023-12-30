7b66e75541a1449d2e0f7a9e23f391358aa0fa59
henriqueambrosano <henriqueambrosano@hotmail.com>
2023-12-30 13:47:47 -0300

    adicionando botao voltar na pagina de loginSettings

--- src/main/loginSettings/index.tsx
+++ src/main/loginSettings/index.tsx
@@ -7,6 +7,9 @@ import {useNavigate} from "react-router-dom";
 import {FormFields, FormState} from "./contracts";
 import { useDispatch } from 'react-redux';
 import { update } from '../../store/slices/startForm';
+import { MenuOption } from '../../components/menuOption';
+import { faBackward } from '@fortawesome/free-solid-svg-icons';
+import { BackButton } from './styles';
 export const LoginSettings = (): JSX.Element => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
@@ -58,48 +61,53 @@ export const LoginSettings = (): JSX.Element => {
         return regex.test(value);
     }
         return (
-      <FormWrapper>
-          <Card
-              bg='light'
-              className='w-50'
-              >
-              <FormTitle className='text-center'>Área exclusiva para administradores</FormTitle>
-              <Card.Body>
-                  <Form onSubmit={handleSubmit}>
-                      <Form.Group className='mb-3' controlId='formBasicName'>
-                          <Form.Label className='fw-bold'>Digite seu email</Form.Label>
-                          <Form.Control
-                              type='text'
-                              name='login'
-                              placeholder='E-mail' 
-                              value={loginForm.fields.login.value}
-                              isValid={loginForm.fields.login.isValid}
-                              isInvalid={!loginForm.fields.login.isValid && loginForm.fields.login.validated}
-                              onChange={(e) => handleInputChange(e.target.name,e.target.value, /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)}
-                              onBlur={(e) => handleInputChange(e.target.name,e.target.value, /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)}
-                          />
-                      </Form.Group>
-                      <Form.Group className='mb-3' controlId='formBasicTeamName'>
-                          <Form.Label className='fw-bold'>Digite sua senha</Form.Label>
-                          <Form.Control 
-                              type='password' 
-                              name='password'
-                              placeholder='Senha'
-                              value={loginForm.fields.password.value}
-                              isValid={loginForm.fields.password.isValid}
-                              isInvalid={!loginForm.fields.password.isValid && loginForm.fields.password.validated}
-                              onChange={(e) => handleInputChange(e.target.name,e.target.value, /^.{5,}$/)}
-                              onBlur={(e) => handleInputChange(e.target.name,e.target.value, /^.{5,}$/)}
-                          />
-                      </Form.Group>
-                      <div className='w-100 d-flex justify-content-center align-items-center mt-5 mb-3 '>
-                          <ButtonStyled
-                              content='Entrar'
-                          />
-                      </div>
-                  </Form>
-              </Card.Body>
-          </Card>
-      </FormWrapper>
-  )
+            <div className=' w-100 h-100 d-flex flex-column'>
+                <FormWrapper>
+                    <Card
+                        bg='light'
+                        className='w-50'
+                        >
+                        <FormTitle className='text-center'>Área exclusiva para administradores</FormTitle>
+                        <Card.Body>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group className='mb-3' controlId='formBasicName'>
+                                    <Form.Label className='fw-bold'>Digite seu email</Form.Label>
+                                    <Form.Control
+                                        type='text'
+                                        name='login'
+                                        placeholder='E-mail' 
+                                        value={loginForm.fields.login.value}
+                                        isValid={loginForm.fields.login.isValid}
+                                        isInvalid={!loginForm.fields.login.isValid && loginForm.fields.login.validated}
+                                        onChange={(e) => handleInputChange(e.target.name,e.target.value, /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)}
+                                        onBlur={(e) => handleInputChange(e.target.name,e.target.value, /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)}
+                                        />
+                                </Form.Group>
+                                <Form.Group className='mb-3' controlId='formBasicTeamName'>
+                                    <Form.Label className='fw-bold'>Digite sua senha</Form.Label>
+                                    <Form.Control 
+                                        type='password' 
+                                        name='password'
+                                        placeholder='Senha'
+                                        value={loginForm.fields.password.value}
+                                        isValid={loginForm.fields.password.isValid}
+                                        isInvalid={!loginForm.fields.password.isValid && loginForm.fields.password.validated}
+                                        onChange={(e) => handleInputChange(e.target.name,e.target.value, /^.{5,}$/)}
+                                        onBlur={(e) => handleInputChange(e.target.name,e.target.value, /^.{5,}$/)}
+                                        />
+                                </Form.Group>
+                                <div className='w-100 d-flex justify-content-center align-items-center mt-5 mb-3 '>
+                                    <ButtonStyled
+                                        content='Entrar'
+                                        />
+                                </div>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </FormWrapper>
+                <BackButton className='ml-1' onClick={() => window.history.back()}>
+                    <MenuOption optionText="Voltar" color="white" icon={faBackward}/>
+                </BackButton>
+            </div>
+        )
 }
